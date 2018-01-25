256153466a8fea2ab80f2344fb83e1fbcfac53bf
ravi4theja <ravi4theja@gmail.com>
2018-01-25 15:30:59 -0600

    refactoring

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui/styles';
-import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import { CSSTransition } from 'react-transition-group';
 
 import * as util from '../../utils';
 
 const styles = theme => ({
-  post: {
-    margin: 5,
-    padding: 20,
-    height: '100%',
-    position: 'relative',
-  },
-  paper: {
-    padding: 20,
-    height: '100%',
-    boxShadow: '0px 0px 83px -8px rgba(0,0,0,0.75)',
-    borderRadius: '5px 5px 5px 5px'
-  },
   title: {
     margin: 5,
     padding: '10px 20px',
@@ -29,13 +16,6 @@ const styles = theme => ({
     padding: 20,
     height: '90%',
     overflow: 'hidden'
-  },
-  btns: {
-    display: 'flex',
-    flexDirection: 'column',
-    position: 'absolute',
-    right: 50,
-    top: '45%'
   }
 })
 
@@ -43,17 +23,7 @@ let scrollIntervalId, setTimeoutIdToTop, setTimeoutIdToBottom;
 
 class Post extends Component {
 
-  state = {containerHeight: 0, containerWidth: 0}
-
   mediaEl = null;
-
-  handleClickNext = () => {
-    this.props.onClickNext();
-  }
-
-  handleClickNSFW = () => {
-    this.props.onClickNSFW(this.props.post.data.id);
-  }
 
   scrollToBottom = () => {
     scrollIntervalId = setInterval(() => {
@@ -76,33 +46,26 @@ class Post extends Component {
     }, 20)
   }
 
-  setImageStyles = (image) => {
-    let h, w
-
-    if (image.height > image.width && image.height > this.state.containerHeight * 1.5){
-      w = Math.min(image.width, this.state.containerWidth)
-    } else {
-      h = Math.min(image.height, this.state.containerHeight)
+  setMediaStyles = media => {
+    let height, width;
+    if(this.mediaEl) {
+      if (media.height > media.width && media.height > this.mediaEl * 0.8 * 1.5){
+        width = Math.min(media.width, this.mediaEl.clientWidth * 0.8)
+      } else {
+        height = Math.min(media.height, this.mediaEl.clientHeight * 0.8)
+      }
     }
-
+    
     return {
       margin: 2,
-      height: h, 
-      width: w,
+      height,
+      width,
       borderRadius: 2 
     }
   }
 
-  handleClickFullScreen = () => {
-    document.getElementById('root').webkitRequestFullscreen()
-  }
-
   componentDidMount() {
-    const container = document.getElementById('bodyContainer')
-    this.setState({
-      containerHeight: container.clientHeight * 0.8,
-      containerWidth: container.clientWidth * 0.8
-    })
+    this.forceUpdate();
     this.scrollToBottom();
   }
 
@@ -114,39 +77,32 @@ class Post extends Component {
   }
 
   render() {
-    const classes = this.props.classes;
+    const { classes } = this.props;
     const postData = this.props.post.data;
     const media = postData.preview? util.isGIF(postData.preview.images[0]): undefined;
     
     return (
-        <CSSTransition
-          timeout={1000}
-          classNames='fade'
-          in={this.props.in}
-          appear={true}
-        >
-          <div className={classes.post} key={postData.id}>
-            <div className={classes.paper}>
-              <Typography type='title' className={classes.title}>{postData.title}</Typography>
-              <div className={classes.content} id='bodyContainer' ref={e => this.mediaEl = e}>
-                {media && 
-                  <img 
-                    style={this.setImageStyles(media)} 
-                    key={media.url} 
-                    src={media.url} 
-                    alt='lol' 
-                  />
-                }
-                {postData.selftext_html && <div dangerouslySetInnerHTML={{__html: postData.selftext_html}}></div>}
-              </div>             
-            </div>
-            <div className={classes.btns}>
-              <Button raised onClick={this.handleClickNext}>NEXT</Button>
-              <Button raised style={{marginTop: 15}} onClick={this.handleClickNSFW}>NSFW?</Button>
-              <Button raised style={{marginTop: 15, display: document.webkitIsFullScreen? 'none': 'block'}} onClick={this.handleClickFullScreen}>FullScreen</Button>
-            </div>   
-          </div>
-        </CSSTransition>         
+      <CSSTransition
+        timeout={1000}
+        classNames='fade'
+        in={this.props.show}
+        appear={true}
+      >       
+        <div style={{width: '100%', height: '100%'}} key={postData.id}>
+          <Typography type='title' className={classes.title}>{postData.title}</Typography>
+          <div className={classes.content} ref={el => {this.mediaEl = el}}>
+            {media && 
+              <img 
+                style={this.setMediaStyles(media)} 
+                key={media.url} 
+                src={media.url} 
+                alt='lol' 
+              />
+            }
+            {postData.selftext_html && <div dangerouslySetInnerHTML={{__html: postData.selftext_html}}></div>}
+          </div>      
+        </div>
+      </CSSTransition>      
     )
   }
 } 
